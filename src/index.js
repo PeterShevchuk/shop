f1e2eea533fc6230dbe304c46c5f911dc9387c5e
Peter Shevchuk <raf02041994@gmail.com>
2020-08-05 18:36:56 +0300

    add timer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './css/styles.css';
+import "./css/styles.css";
+
+import Timer from "./js/timer.js";
+
+const timer1 = new Timer({
+  day: ".saleTop__timer-day p",
+  hour: ".saleTop__timer-hour p",
+  min: ".saleTop__timer-min p",
+  sec: ".saleTop__timer-sec p",
+  targetDate: new Date("Sep 1, 2020"),
+});
+timer1.startTimer();
